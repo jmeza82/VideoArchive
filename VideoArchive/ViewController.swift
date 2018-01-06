042966a2fe3f6acce0c9874d4b07334ff612bcb2
Juan Meza <juanmeza@Juans-MacBook-Pro.local>
2018-01-05 19:34:59 -0800

    Initial Commit

---
+++ VideoArchive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoArchive
+//
+//  Created by Juan Meza on 1/5/18.
+//  Copyright © 2018 Tech-IN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
